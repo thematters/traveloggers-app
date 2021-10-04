6dd0df3383b26571cb506dba6b5cfa23ac8e8738
TomasC <49659410+tx0c@users.noreply.github.com>
2021-10-04 18:14:16 +0000

    initialize with gatsby4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # matters-nft
 matters-nft landing page website
+
+initialized with gatsby 4 starters
---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+// If you don't want to use TypeScript you can delete this file!
+import * as React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+type DataProps = {
+  site: {
+    buildTime: string
+  }
+}
+
+const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
+  <Layout>
+    <Seo title="Matters NFT" />
+    <h1>Matters NFT</h1>
+    <p>
+      Matters NFT
+    </p>
+    <p>
+      You're currently on the page "{path}" which was built on{" "}
+      {data.site.buildTime}.
+    </p>
+  </Layout>
+)
+
+export default UsingTypescript
+
+export const query = graphql`
+  {
+    site {
+      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
+    }
+  }
+`
