1b7acaa4a7f40b472e73e8b345028accde5f83ea
robertu <4065233+robertu7@users.noreply.github.com>
2021-10-19 14:39:11 +0800

    poc of login and logout

--- src/components/Viewer/index.tsx
+++ src/components/Viewer/index.tsx
@@ -1,23 +1,118 @@
-import { useQuery } from "graphql-hooks"
+import { useFormik } from "formik"
+import { useMutation, useQuery } from "graphql-hooks"
+import * as React from "react"
 
-const VIEWER_QUERY = `query ViewerQuery {
+interface FormValues {
+  email: string
+  password: ""
+}
+
+const VIEWER_QUERY = `
+  query ViewerQuery {
     viewer {
       id
       displayName
       userName
+      avatar
     }
-  }`
+  }
+`
+
+const LOGIN = `
+  mutation UserLogin($input: UserLoginInput!) {
+    userLogin(input: $input) {
+      auth
+    }
+  }
+`
+
+const LOGOUT = `
+  mutation UserLogout {
+    userLogout
+  }
+`
 
 export const Viewer: React.FC = () => {
+  const [login] = useMutation(LOGIN)
+  const [logout, { loading: loggingOut }] = useMutation(LOGOUT)
+
   const { loading, error, data } = useQuery(VIEWER_QUERY)
 
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    isValid,
+    isSubmitting,
+  } = useFormik<FormValues>({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async ({ email, password }, { setSubmitting }) => {
+      try {
+        await login({ variables: { input: { email, password } } })
+        window.location.reload()
+      } catch (err) {
+        console.error(err)
+        setSubmitting(false)
+      }
+    },
+  })
+
   if (loading) {
-    return "loading"
+    return <span>Loading</span>
   }
 
   if (error) {
-    return "error"
+    return <span>{JSON.stringify(error, null, 2)}</span>
   }
 
-  return data.viewer.userName
+  if (!data.viewer.userName) {
+    return (
+      <form method="POST" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          required
+          placeholder="Email"
+          value={values.email}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          required
+          placeholder="Password"
+          value={values.password}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+
+        <button type="submit" disabled={isSubmitting || !isValid}>
+          Login
+        </button>
+      </form>
+    )
+  }
+
+  return (
+    <div>
+      <span>{data.viewer.userName}</span>
+      <img src={data.viewer.avatar} />
+      <button
+        type="button"
+        onClick={async () => {
+          await logout()
+          window.location.reload()
+        }}
+        disabled={loggingOut}
+      >
+        {loggingOut ? <span>Logging out</span> : <span>Logout</span>}
+      </button>
+    </div>
+  )
 }
