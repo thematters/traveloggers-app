eeebb2270c7cc98eacefabc3299a5dde1e262108
TomasC <49659410+tx0c@users.noreply.github.com>
2021-10-25 01:10:08 +0000

    feat(char-intro): [NFT Page] - Character intro 2pt feature request
    resolves #21

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -3,17 +3,27 @@ import React from "react"
 
 import { SEO } from "~/components"
 import Benefits from "~/views/Homepage/Benefits"
+import CharacterIntro from "~/views/Homepage/CharacterIntro"
 import Hero from "~/views/Homepage/Hero"
 import Questions from "~/views/Homepage/Questions"
 import Roadmap from "~/views/Homepage/Roadmap"
 
-const Homepage: React.FC<PageProps> = () => {
+type DataProps = {
+  site: {
+    buildTime: string
+  }
+}
+
+const Homepage: React.FC<PageProps<DataProps>> = ({
+  pageContext: { locale, originalPath },
+}) => {
   return (
     <>
-      <SEO />
+      <SEO title="CryptoMatties" lang={locale} />
 
       <Hero />
       <Roadmap />
+      <CharacterIntro />
       <Benefits />
       <Questions />
     </>
