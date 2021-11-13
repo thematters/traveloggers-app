f9d885b8cc6ffedb8e494305edba84c8250b88cf
robertu <4065233+robertu7@users.noreply.github.com>
2021-11-13 17:49:38 +0800

    remove "query-string" and "axiso"

--- src/utils/openSea.ts
+++ src/utils/openSea.ts
@@ -1,6 +1,3 @@
-import axios from "axios"
-import queryString from "query-string"
-
 import env from "@/.env.json"
 
 const OPENSEA_API_BASE_URL =
@@ -15,16 +12,17 @@ type OpenSeaAsset = {
 export const retrieveOwnerNFTs = async ({ owner }: { owner: string }) => {
   const contractAddress = env.contractAddress
 
-  const qs = queryString.stringify({
+  const searchParams = new URLSearchParams({
     owner,
     asset_contract_address: contractAddress,
     order_direction: "desc",
-    offset: 0,
-    limit: 50, // TODO: pagination support
+    offset: "0",
+    limit: "50", // TODO: pagination support
   })
-  const url = `${OPENSEA_API_BASE_URL}/assets?${qs}`
 
-  const result = await axios.get(url)
+  const url = `${OPENSEA_API_BASE_URL}/assets?${searchParams.toString()}`
+
+  const result = await fetch(url).then(res => res.json())
 
   return (result?.data?.assets || []) as OpenSeaAsset[]
 }
