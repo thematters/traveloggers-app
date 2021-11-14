462e113f31b3291a5a9a538e3190a0695c490322
Zeck Li <devformatters@gmail.com>
2021-11-15 01:33:30 +0800

    Add a new Roadmap and move old to timeline folder

--- src/views/Homepage/Roadmap/texts.tsx
+++ src/views/Homepage/Roadmap/texts.tsx
@@ -1,193 +1,66 @@
-import React from "react"
-
 const TEXTS: Record<string, any> = {
   zh: {
-    title: "2021 計畫時程",
-
-    // pre-order
-    event1: "預購",
-    content1: (
-      <>
-        此次預購數量一共為 300 個，價格為 <b>0.03 ETH</b>。
-        如果預購成功，你的以太坊錢包地址將會獲得一個隨機的 Traveloggers 盲盒。
-        此 Traveloggers 盲盒將會在 2021/11/12 中午揭曉。每個錢包地址限購 5 個
-        Traveloggers。
-      </>
-    ),
-    event1_1: "開放預購",
-    event1_1_content: (
-      <section>
-        <p>
-          <b>11/05</b>
-          <span>&nbsp;UTC+8:00 中午 12:00&nbsp;&mdash;&nbsp;</span>
-        </p>
-        <p>
-          <b>11/11</b>
-          <span>&nbsp;UTC+8:00 中午 12:00</span>
-        </p>
-      </section>
-    ),
-
-    event1_2: "盲盒揭曉",
-    event1_2_content: (
-      <section>
-        <p>
-          <b>11/12</b>
-          <span>&nbsp;UTC+8:00 中午 12:00&nbsp;&mdash;&nbsp;</span>
-        </p>
-        <p>
-          <b>11/12</b>
-          <span>&nbsp;UTC+8:00 下午 14:00</span>
-        </p>
-      </section>
-    ),
-    event1_button_upcoming: "尚未開始",
-    event1_button_open: "參與預購",
-    event1_button_closed: "已結束",
-    event1_button_sold: "預購已全數售罄",
-
-    // airdrop
-    event2: "空投",
-    content2: (
-      <p>
-        為感謝{" "}
-        <a href="https://matters.news/" target="_blank" rel="noreferrer">
-          Matters.News
-        </a>{" "}
-        社區用戶支持，我們會空投 300 個 Traveloggers 給綁定以太坊錢包的 Matters
-        用戶。只要在 2021/11/11 中午 12 點以前成功綁定了以太坊錢包的 Matters
-        用戶都有機會獲得空投。
-      </p>
-    ),
-    event2_1: "開放登記",
-    event2_1_content: (
-      <section>
-        <p>
-          <b>11/05</b>
-          <span>&nbsp;UTC+8:00 中午 12:00&nbsp;&mdash;&nbsp;</span>
-        </p>
-        <p>
-          <b>11/11</b>
-          <span>&nbsp;UTC+8:00 中午 12:00</span>
-        </p>
-      </section>
-    ),
-    event2_2: "執行空投",
-    event2_2_content: (
-      <section>
-        <p>
-          <b>11/12</b>
-          <span>&nbsp;UTC+8:00 中午 12:00&nbsp;&mdash;&nbsp;</span>
-        </p>
-        <p>
-          <b>11/12</b>
-          <span>&nbsp;UTC+8:00 下午 14:00</span>
-        </p>
-      </section>
-    ),
-    event2_button_upcoming: "尚未開始",
-    event2_button_open: "參與空投",
-    event2_button_closed: "已結束",
-
-    // open sale
-    event3: "OpenSea 公開發售",
+    title: "發展路線",
+    nov: "11 月",
+    dec: "12 月",
+    jan: "1 月",
+    feb: "2 月",
+    item1: "預購 / 空投 / 公開發售",
+    content1: "Traveloggers 預購、空投和在 OpenSea 上公開發售。",
+    item2: "個人頭像",
+    content2:
+      "Traveloggers 擁有者可以在 Matters.News 和 Twitter 上換帶有 NFT 標識認證的個人頭像。",
+    item3: "航行日誌",
     content3:
-      "2021/11/15 起，Traveloggers 將會在 OpenSea 公開發售，發售價從 0.06 ETH 起。",
-    event3_1: "開始時間",
-    event3_1_content: (
-      <section>
-        <p>
-          <b>11/15</b>
-          <span>&nbsp;UTC+8:00 中午 12:00</span>
-        </p>
-      </section>
-    ),
-    event3_button_upcoming: "尚未開始",
-    event3_button_open: "進入 OpenSea 購買",
-    event3_button_closed: "已結束",
+      "Traveloggers 擁有者可以寫入航行日誌，航行日誌的所有權可以與頭像一起移轉。",
+    item4: "POAP",
+    content4: "未獲得空投的參與者可獲得紀念 POAP。",
+    item5: "禮物贈送",
+    content5_1: "Traveloggers 所有權可在 ",
+    content5_2: "https://traveloggers.matters.news/ ",
+    content5_3: "直接贈送。",
+    item6: "ENS 整合",
+    content6: "Traveloggers Web App 通過 IPFS 存儲，並能通過 ENS 域名訪問。",
+    item7: "The Space",
+    content7: "Traveloggers 擁有者將會獲得下一個大型 NFT 項目 The Space 空投。",
+    item8: "Family Tree",
+    content8:
+      "Traveloggers 擁有者有權參與新人物和故事共創：參與 Family Tree 繁衍計劃以及獲得新一輪人物空投。",
+    item9: "社區 Token 空投",
+    content9: "Traveloggers 持有者將會獲得社區 Token 空投。",
   },
   en: {
-    title: "2021 Timeline",
-
-    // pre-order
-    event1: "Pre-orders",
-    content1: (
-      <>
-        The pre-order amount is 300, and the price is <b>0.03 ETH</b>. If the
-        pre-order is successful, you will receive a random blind box of
-        Traveloggers, which will be revealed at noon on November 12, 2021. Each
-        wallet address can purchase up to 5 Traveloggers.
-      </>
-    ),
-    event1_1: "Open",
-    event1_1_content: (
-      <section>
-        <b>12pm November 5 &mdash; 12pm November 11, 2021</b>
-        <span>&nbsp;(UTC+8)</span>
-      </section>
-    ),
-    event1_2: "Reveal",
-    event1_2_content: (
-      <section>
-        <b>12 &mdash; 2pm, November 12, 2021</b>
-        <span>&nbsp;(UTC+8)</span>
-      </section>
-    ),
-    event1_button_upcoming: "Upcoming",
-    event1_button_open: "Participate",
-    event1_button_closed: "Closed",
-    event1_button_sold: "Pre-order Sold Out",
-
-    // airdrop
-    event2: "Airdrops",
-    content2: (
-      <>
-        To thank{" "}
-        <a href="https://matters.news/" target="_blank" rel="noreferrer">
-          Matters.News
-        </a>{" "}
-        users’ support, we will airdrop 300 Traveloggers to Matters’ users who
-        have connected their Ethereum wallets. Matters’ users who have connected
-        their Ethereum wallets before noon on November 11, 2021 will have the
-        opportunity to receive the airdrop.
-      </>
-    ),
-    event2_1: "Open",
-    event2_1_content: (
-      <section>
-        <b>12pm November 5 &mdash; 12pm November 11, 2021</b>
-        <span>&nbsp;(UTC+8)</span>
-      </section>
-    ),
-    event2_2: "Execute",
-    event2_2_content: (
-      <section>
-        <b>12 &mdash; 2pm, November 12, 2021</b>
-        <span>&nbsp;(UTC+8)</span>
-      </section>
-    ),
-    event2_button_upcoming: "Upcoming",
-    event2_button_open: "Participate",
-    event2_button_closed: "Closed",
-
-    // open sale
-    event3: "Public Sale On OpenSea",
-    content3: (
-      <>
-        Starting November 15, 2021, Traveloggers will be available for sale on
-        OpenSea from 0.06 ETH.
-      </>
-    ),
-    event3_1: "Open",
-    event3_1_content: (
-      <section>
-        <b>12pm November 15, 2021</b>
-        <span>&nbsp;(UTC+8)</span>
-      </section>
-    ),
-    event3_button_upcoming: "Upcoming",
-    event3_button_open: "Enter OpenSea to purchase",
-    event3_button_closed: "Closed",
+    title: "Roadmap",
+    nov: "Nov",
+    dec: "Dec",
+    jan: "Jan",
+    feb: "Feb",
+    item1: "Pre-order / Airdrops / Public Sale",
+    content1: "Traveloggers’ pre-order, airdrop, and public sale on OpenSea.",
+    item2: "Profile Picture",
+    content2:
+      "Traveloggers’ owners can set the Traveloggers NFT as their Twitter and Matters.News profile picture.",
+    item3: "The Logbook",
+    content3:
+      "Traveloggers’ owners can write a logbook that comes with the NFT, the ownership of the logbook also can be transferred with the Traveloggers.",
+    item4: "POAP Souvenir",
+    content4:
+      "Airdrop participants who do not receive airdrop will receive a POAP souvenir.",
+    item5: "Transfer as A Gift",
+    content5: "Traveloggers can be directly transferred as a gift.",
+    item6: "ENS Integration",
+    content6:
+      "The Traveloggers Web App will be stored on IPFS, and can be accessed through ENS domains.",
+    item7: "The Space",
+    content7:
+      "Traveloggers’ owners will receive airdrops of the next large-scale NFT project — The Space.",
+    item8: "Family Tree",
+    content8:
+      "Traveloggers’ owners can vote to decide on creating new characters and stories—the Family Tree reproduction" +
+      ", and receive a new round of avatar airdrops.",
+    item9: "Community Token Airdrops",
+    content9:
+      "Traveloggers’ owners will have a chance to receive community Token airdrops.",
   },
 }
 
