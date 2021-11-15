ba120c473c48185d259833f207d9ade4adb6c0f4
tx0c <>
2021-11-15 12:41:34 +0000

    change logbooks text description

--- src/views/Logbook/LogbookList.tsx
+++ src/views/Logbook/LogbookList.tsx
@@ -71,6 +71,8 @@ const LogbookListContentItem: React.FC<ItemProps> = ({ logbook }) => {
 }
 
 const LogbookListContent = () => {
+  const { locale } = useLocalization()
+
   const searchInputRef = useRef<HTMLInputElement>(null)
   const [searchTerm, setSearchTerm] = useState("")
   // const [searchToken, setSearchToken] = useState(null)
@@ -146,12 +148,46 @@ const LogbookListContent = () => {
       ) : (
         <>
           <section className={styles.text}>
-            <Section.Title>Create memory in Matterverse</Section.Title>
+            <Section.Title>
+              {locale === Lang.en ? "Record in your Logbook" : "寫下你的航行日誌"}
+            </Section.Title>
             <Section.Content>
-              <p>Write your chapter and pass it on to the next person</p>
               <p>
-                The current owner of the Avatar can unlock all the text in the
-                Logbook
+                {locale === Lang.en ? (
+                  <>
+                    The owners of Traveloggers have the opportunities to record
+                    in the Logbook. The Logbook and its contents will be passed
+                    down to successive Travelogges owners.
+                  </>
+                ) : (
+                  <>
+                    Traveloggers的擁有者，可以有一次機會寫下日誌。日誌本及其中的內容，將隨著
+                    Travelogges的所有權一起轉移
+                  </>
+                )}
+              </p>
+              <p>
+                {locale === Lang.en ? (
+                  <>
+                    Logbook 1.0 is stored on the Ethereum mainnet. An Ethereum
+                    miner fee(gas fee) is required when writing in the logbook.
+                  </>
+                ) : (
+                  <>
+                    第一代日誌本保存在以太坊的主網。寫入日誌時需要繳納以太坊礦工費（Gas
+                    Fee）
+                  </>
+                )}
+              </p>
+              <p>
+                {locale === Lang.en ? (
+                  <>
+                    Please feel free to write down anything that can represent
+                    yourself or you want to express.
+                  </>
+                ) : (
+                  <>請寫下你能代表自己，亦想向世界傳達的，最重要的文字</>
+                )}
               </p>
             </Section.Content>
           </section>
